3b1648ea390cdb9da2bb8bdea74c5ccc41d8fc44
Souza <olsouza.dev@gmail.com>
2026-01-07 18:36:47 -0300

    aulas 1, 2 e 3 concluidas

---
+++ aula2/comentarios.js
@@ -0,0 +1,22 @@
+// Meu comentário bacana
+console.log('Olá mundo!') // Outro comentário
+console.log('Linha 1');
+
+/* 
+Um bloco de código
+Linha de comentário 
+*/
+
+/**
+ * Um Título
+ * 
+ * 
+ * Lorem
+ */
+
+//console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+// Vai ser ignorado
