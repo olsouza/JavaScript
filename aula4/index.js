8fa5685a1054abfb5277f16bbf716d66d7bf5fe9
Souza <olsouza.dev@gmail.com>
2026-01-08 20:39:35 -0300

    aula 4 finalizada

---
+++ aula4/index.js
@@ -0,0 +1,16 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variávies com let
+// NÃO UTILIZE VAR UTILIZE LET.
+
+let nome; // Declarou a variável (String)
+nome = 'Henrique'; // Inicializando a variável
+console.log(nome, 'nasceu em 2009.');
+console.log('em 2011', nome, 'jogou bola.');
+nome = 'Luis';
+console.log(nome, 'criou um canal no youtube em 2017.');
+console.log('O cachorro de', nome, 'se chama Luke.');
