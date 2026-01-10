32ccac9c434de57d3e6e258031108ac596225445
Souza <olsouza.dev@gmail.com>
2026-01-09 22:38:09 -0300

    aula 9 finalizada

---
+++ aula9/index.js
@@ -0,0 +1,24 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação (- / *)
+ * ** Potenciação
+ * % Resto da divisão
+ * Precedências:
+ * ()
+ * **
+ * * / %
+ * + -
+ * Incrimento = ++
+ * Descremento = --
+ * 
+ * Operadores de atribuição
+ * 
+ * NaN = Not a Number ParseInt (inteiro) ParseFloat (decimais)
+ *  */ 
+
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
+console.log(typeof num2);
+
+
