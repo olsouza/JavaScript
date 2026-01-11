3398dfc5148b0692e22761f5669cdb9d87042716
Souza <olsouza.dev@gmail.com>
2026-01-10 23:02:03 -0300

    aula 11 parte 1 completa

---
+++ aula11/index.js
@@ -0,0 +1,5 @@
+//               012345679
+let umaString = "O rato roeu a ropa do rei de roma.";
+// .slice => (selecinona palavras)  .length => (tamanho das palavras)  .replace => (trocar palavras)  .split => (separa palavras)
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
