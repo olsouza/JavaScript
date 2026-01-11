ddfa94f7c1ae06238165ec517f24952dd40442d3
Souza <olsouza.dev@gmail.com>
2026-01-10 23:50:06 -0300

    exercício finalizado

--- aula11/index.js
+++ aula11/index.js
@@ -1,5 +1,6 @@
 //               012345679
 let umaString = "O rato roeu a ropa do rei de roma.";
-// .slice => (selecinona palavras)  .length => (tamanho das palavras)  .replace => (trocar palavras)  .split => (separa palavras)
+// .slice => (selecinona palavras)  .length => (tamanho das palavras)  .replace => (trocar palavras)  .split => (separa palavras) .search  .match
 console.log(umaString.toUpperCase());
 console.log(umaString.toLowerCase());
+console.log(umaString.match(2));
