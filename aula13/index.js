f9fbefdc9c5e590f162fe39dd9a4893e98b21fc7
Souza <olsouza.dev@gmail.com>
2026-01-11 00:49:19 -0300

    aula 13 finalizada

---
+++ aula13/index.js
@@ -0,0 +1,8 @@
+console.log(!!(100 / 0));
+
+// let num2 = Math.floor(num1); arredonda para baixo
+// let num2 = Math.ceil(num1); arredonda para cima
+// let num2 = Math.round(num1); arredonda para o mais próximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,20)); maior número
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,20)); menor número
+//const aleatorio = Math.round(Math.random() * (10 - 0) + 0); números aleatórios
