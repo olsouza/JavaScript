c080ccec6a2dde28a6a35375ccbd78abfc97ed51
Souza <olsouza.dev@gmail.com>
2026-01-11 23:37:47 -0300

    aula 15 finalizada

---
+++ aula15/index.js
@@ -0,0 +1,16 @@
+//  function soma(x = 1, y = 1) {
+//      const resultado = x + y;
+//      return resultado;
+//  
+//  const resultado = soma(4, 2);
+//  console.log(resultado);
+
+const raiz = function (n) {
+    return n ** 0.5;
+}
+
+console.log(raiz(25));
+console.log(raiz(9));
+console.log(raiz(144));
+
+// const raiz = n => n ** 0.5;   FUNCTION ARROW
