052577d16324b5426b141f6a4542a6f4acc8f8b0
Souza <olsouza.dev@gmail.com>
2026-01-11 23:17:14 -0300

    aula 14 completa

---
+++ aula14/index.js
@@ -0,0 +1,38 @@
+//            0123456891011
+const nome = 'Luis Henrique'
+
+//               0       1        2          3       4
+const alunos = ['Luis', 'Maria', 'João']; // Luisa,  Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luisa');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, -3));   SELECIONAR
+
+// console.log(alunos[50]);   UNDEFINED
+
+// delete alunos[1];    DELETA
+// console.log(alunos);
+
+// const removido = alunos.shift(); OU const removido = alunos.pop();   REMOVER ARRAY DO INICIO OU FIM
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luisa');    ADICIONAR NO COMEÇO
+// alunos.unshift('Lucas');
+
+// alunos.push('Luisa');     ADICIONAR NO FIM
+// alunos.push('Carlos');
+
+// alunos[alunos.length] = 'Luisa';  ADICIONAR NO FIM
+// alunos[alunos.length] = 'Jose';
+// alunos[alunos.length] = 'Mateus';
+
+// alunos[3] = 'Luisa';      ADICIONAR
+// alunos[0] = 'Lucas';      ALTERA
+
+// console.log(alunos);
+// console.log(alunos[0]);      SELECIONAR
+// console.log(alunos[2]);
