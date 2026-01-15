b79930c6ba3a39d54cd12bd7fd31717cb9281c73
Souza <olsouza.dev@gmail.com>
2026-01-14 21:39:47 -0300

    aula sobre operadores lógicos

---
+++ aula20/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OUR
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true  && true;
+// const expressaoOr = true || false || false || false;
+
+// const usuario = 'Luis'; // form usuário digitou
+// const senha = '123456'; // form usuário digitou
+// 
+//                          true                false
+// const vaiLogar = usuario === 'Luis' && senha === '12345';
+// console.log(vaiLogar);
+
+console.log(!!false);
