67fe0669780237623a95e7d46802128eef03cbfb
Souza <olsouza.dev@gmail.com>
2026-01-17 16:04:23 -0300

    Create index.js

---
+++ aula28/index.js
@@ -0,0 +1,39 @@
+const verdadeira = true;
+
+// let tem escopo de bloco {... bloco}
+// var so tem escopo de função
+
+// let nome = 'Luis'; // criando
+// var nome2 = 'Luis'; // criando
+
+
+// if (verdadeira) {
+//     let nome = 'Henrique'; // criando
+//     var nome2 = 'Henrique'; // Redeclarando
+
+//     if (verdadeira) {
+//     let nome = 'Souza';
+//     var nome2 = 'Souza'; // Redeclarando
+//     }
+// }
+
+// console.log(nome, nome2);
+
+
+
+// function falaOi () {
+//     let sobrenome = 'Henrique';
+
+//     if (verdadeira) {
+//         let sobrenome = 'Luis';
+//         console.log(sobrenome);
+//     }
+
+//     console.log(sobrenome);
+// }
+
+// falaOi();
+
+
+let sobrenome = 'Luis';
+console.log(sobrenome);
