11b7f41a741345dec836561a6bb48bf94f5a86a0
Souza <olsouza.dev@gmail.com>
2026-01-17 17:07:42 -0300

    Create index.js

---
+++ aula31/index.js
@@ -0,0 +1,9 @@
+//               0       1       2     ...
+const frutas = ['Maça', 'Pêra', 'Uva', 'Luis', 'Lucas'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
+
+// const par = i % 2 === 0 ? 'par' : 'ímpar';
+// console.log(i, par)
