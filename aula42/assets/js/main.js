5b28488bc381d98e485d536e510bf61a6f09bb6f
Souza <olsouza.dev@gmail.com>
2026-01-20 21:02:04 -0300

    criei um cronômetro

---
+++ aula42/assets/js/main.js
@@ -0,0 +1,45 @@
+function relogio() {
+
+    function getTimeFromSeconds(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = getTimeFromSeconds(segundos);
+    }, 1000);
+}
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    
+    if(el.classList.contains('zerar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    }
+
+    if(el.classList.contains('pausar')) {
+        relogio.classList.add('pausado');
+        clearInterval(timer);
+    }
+
+    if(el.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+
+ });
+}
+relogio();
