bc3aba50919b398ab81cbdfa531c82fe4c549e76
Souza <olsouza.dev@gmail.com>
2026-01-23 22:00:22 -0300

    function return

---
+++ aula46/index.js
@@ -0,0 +1,34 @@
+// return
+// Retorna um valor
+// Termina a função
+
+// document.addEventListener('click', function() {
+//     document.body.style.backgroundColor = 'red';
+// });
+
+// function falaFrase(comeco) {
+//     function falaResto(resto) {
+//         return comeco + ' ' + resto;
+//     }
+//     return falaResto;
+// }
+
+// const fala = falaFrase('Olá');
+// const resto = fala('mundo!');
+// console.log(resto);
+
+function criaMultiplicador(multiplicador) {
+    // multiplicador
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+
+console.log(duplica(4));
+console.log(triplica(4));
+console.log(quadriplica(4));
