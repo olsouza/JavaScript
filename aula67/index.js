71b3caa469a526f8ef5fc9a53439abd8906d8a7d
Souza <olsouza.dev@gmail.com>
2026-01-30 22:24:43 -0300

    Create index.js

---
+++ aula67/index.js
@@ -0,0 +1,41 @@
+// defineProperty -> Getter -> obter valor e Setter -> configurar valor
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    let estoquePrivado = estoque;
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // Mostra a chave
+        configurable: true, // configuravel
+        get: function() {
+            return estoquePrivado;
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') {
+                throw new TypeError('Não é número');
+            }
+
+            estoquePrivado = valor;
+        }
+    });
+}
+
+function criaProduto(nome) {
+    return {
+        get nome() {
+            return nome;
+        },
+        set nome(valor) {
+            valor = valor.replace('coisa.', '');
+            nome = valor;
+        }
+    }
+}
+
+// const p1 = new Produto('Camiseta', 20, 3);
+// console.log(p1);
+// p1.estoque = 500;
+// console.log(p1.estoque)
+const p2 = criaProduto('Camiseta');
+p2.nome = 'Qualquer coisa.';
+console.log(p2.nome)
