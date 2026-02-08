f349d0d05a8f21cd5e3072de79f0e5238cdccc4b
Souza <olsouza.dev@gmail.com>
2026-02-07 23:20:13 -0300

    Create index.js

---
+++ aula83/index.js
@@ -0,0 +1,56 @@
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(typeof msg !== 'string') {
+            reject('CAI NO ERRO');
+            return;
+            }
+
+            resolve(msg.toUpperCase() + ' - Passei na promise');
+            return;
+        }, tempo);
+    });
+}
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+// tenta resolver todas se uma der errado rejeita todas
+// Sempre entrega o primero valor resolvido
+// Retorna uma Promise Resolvida de cara
+// Retorna uma Promise rejeitada de cara
+
+function baixaPagina() {
+    const emCache = true;
+
+    if(emCache) {
+        return Promise.resolve('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch(e => console.log('ERRO', e));
+
+// const promises = [
+//     esperaAi('Promise 1', rand(1, 5)),
+//     esperaAi('Promise 2',rand(1, 5)),
+//     esperaAi('Promise 3', rand(1, 5)),
+//     esperaAi(1000, rand(1, 5))
+// ];
+
+// Promise.race(promises)
+// .then(function(valor) {
+//     console.log(valor);
+// })
+// .catch(function(erro) {
+//     console.log(erro);
+// });
