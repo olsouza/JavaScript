789f68a6f797525d33d257ed356fd169120b0327
Souza <olsouza.dev@gmail.com>
2026-02-18 00:26:31 -0300

    Core-js e regenerator-runtime

---
+++ aula93/src/main.js
@@ -0,0 +1,8 @@
+import 'core-js/stable'; // pra rodar em navegadores antigos
+import 'regenerator-runtime/runtime';
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+
+executa();
---
+++ aula93/src/modules/promises.js
@@ -0,0 +1,15 @@
+import { setTimeout } from "core-js";
+
+function promise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Hey, sou a promise.');
+            resolve();
+        }, 2000);
+    });
+}
+
+export default async function () {
+    await promise();
+    console.log('Terminou');
+}
