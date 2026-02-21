8891c1fdfb13b02878d9f02a783b5a754176b3cd
Souza <olsouza.dev@gmail.com>
2026-02-20 23:09:50 -0300

    Módulos (2)

---
+++ nodes/aula2/app.js
@@ -0,0 +1,19 @@
+const Cachorro = require('./Z/mod2.js');
+const c1 = new Cachorro('dog');
+c1.latir();
+
+// const path = require('path');
+// console.log(__dirname);
+// console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'));
+// console.log(__filename);
+// console.log(__dirname);
+
+// const Cachorro = require('./Z/mod2');
+
+// const c1 = new Cachorro('dog');
+// c1.latir();
+
+// ./B/arquivo
+// ./arquivo.js
+// ../nomepasta/nomearquivo
+// ../arquivo.js
