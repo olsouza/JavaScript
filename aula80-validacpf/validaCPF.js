d32f35b056e97355a49ca57bd8be5c2475fe4c90
Souza <olsouza.dev@gmail.com>
2026-03-11 19:54:19 -0300

    configurei o VScode e adicionei algumas extensões

--- aula80-validacpf/validaCPF.js
+++ aula80-validacpf/validaCPF.js
@@ -2,11 +2,11 @@
 // classe é igual função construtora
 class ValidaCPF {
     constructor(cpfEnviado) {
-        Object.defineProperty(this, 'cpfLimpo', {
+        Object.defineProperty(this, "cpfLimpo", {
             writable: false,
             enumerable: true,
             configurable: false,
-            value: cpfEnviado.replace(/\D+/g, '')
+            value: cpfEnviado.replace(/\D+/g, ""),
         });
     }
 
@@ -25,21 +25,20 @@ class ValidaCPF {
         let total = 0;
         let reverso = cpfSemDigitos.length + 1;
 
-        for(let stringNumerica of cpfSemDigitos) {
+        for (let stringNumerica of cpfSemDigitos) {
             total += reverso * Number(stringNumerica);
             reverso--;
         }
 
         const digito = 11 - (total % 11);
-        return digito <= 9 ? String(digito) : '0';
-
+        return digito <= 9 ? String(digito) : "0";
     }
 
     valida() {
-        if(!this.cpfLimpo) return false;
-        if(typeof this.cpfLimpo !== 'string') return false;
-        if(this.cpfLimpo.length !== 11) return false;
-        if(this.ehSequencia()) return false;
+        if (!this.cpfLimpo) return false;
+        if (typeof this.cpfLimpo !== "string") return false;
+        if (this.cpfLimpo.length !== 11) return false;
+        if (this.ehSequencia()) return false;
         this.geraNovoCpf();
 
         return this.novoCPF === this.cpfLimpo;
